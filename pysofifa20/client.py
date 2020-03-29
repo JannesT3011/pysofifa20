a00d3d1cf31223d02103f154d1e80da98571c4ce
Jannes <36892200+Bmbus@users.noreply.github.com>
2020-03-29 17:11:23 +0200

    Delete testing stuff

--- pysofifa20/client.py
+++ pysofifa20/client.py
@@ -209,6 +209,3 @@ class Pysofifa:
         :rtype: list
         """
         return
-
-t = Pysofifa("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0").get_team_info_by_name("Ajax")
-print(t)
